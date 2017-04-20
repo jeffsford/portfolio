b99e9c2767539732e96e8d06b3db92b9371dc863
jeffsford <jeff@jeffford.com>
2017-04-19 21:41:15 -0700

    Added additional javascript

---
+++ public/js/projectData.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var projectData = [
+  {
+    title:       'Space Station',
+    image:       'images/par.png',
+    link:        'https://jeffsford.github.io/space-station/',
+    deployedOn:  '2017-03-31',
+    body:        'My final group project for the Code Fellows 201 course, Space Station is an educational site for children. It features a homepage that includes links for other space and science related resources, an educational page with images of the planets in our solar system, a quiz that tests the user\'s knowledge of what they learned, as well as a comment section where users can leave their feedback.'
+  },
+  {
+    title:       'BusMall',
+    image:       'images/bus.png',
+    link:        'https://jeffsford.github.io/bus_mall/',
+    deployedOn:  '2017-03-24',
+    body:        'Similar to SkyMall on airplanes, BusMall is a retailer will catalogues in the seat-backs on local buses. This site surveys users by asking them to click on one of three images (which are all BusMall products), based on which item is most appealing to them. Once an image is clicked, three more images appear. This process continues until the user has clicked a total of 25 times. A chart then renders the results of all users who have participated in the survey.'
+  },
+  {
+    title:       'Cookie Stand',
+    image:       'images/cookie.png',
+    link:        'https://jeffsford.github.io/cookie-stand/',
+    deployedOn:  '2017-03-17',
+    body:        'Pat\'s Salmon Cookies is a ficticious cookie chain with several locations in the Seattle area. The site features information about the store including locations, hours, and merchandise available for purchase. Cookie stand features a second page that forecasts average customers each hour at all of it\'s locations. This feature helps the company determine what supplies they need to have on-hand, in order to meet demand.'
+  },
+  {
+    title:       'About Me',
+    image:       'images/cookie.png',
+    link:        'https://jeffsford.github.io/about_me/',
+    deployedOn:  '2017-03-10',
+    body:        'Biographical page that features a quiz based on the information provided. The quiz offers feedback to the user, letting them know if they correctly answered the question.'
+  }
+];
